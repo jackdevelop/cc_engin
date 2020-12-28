93874c98f14d5bead275c38aab0e7b7e4fde5860
bryan sun <hitsjt@gmail.com>
2020-12-28 17:45:51 +0800

    add Roomseat.ts

---
+++ scripts/libs/constants/GameEventConstants.ts
@@ -0,0 +1,89 @@
+
+export class GameEventConstants {
+
+
+  
+  public static onGameResize = 'onGameResize';
+  
+  public static onKeyDown = 'onKeyDown';
+
+  
+  public static SCENE_CHANGE_START_END = 'SCENE_CHANGE_START_END';
+  public static SCENE_CHANGE_START = 'SCENE_CHANGE_START';
+  public static SCENE_CHANGE_END = 'SCENE_CHANGE_END';
+
+  
+  public static PANEL_OPEN_START = 'PANEL_OPEN_START'; 
+  public static PANEL_OPEN_BEGIN = 'PANEL_OPEN_BEGIN';
+  public static PANEL_OPEN = 'PANEL_OPEN'; 
+  public static PANEL_HIDE = 'PANEL_HIDE';
+
+
+  
+  
+  
+  
+
+  
+  public static server_disconnect = 'server_disconnect';
+  
+  public static server_user_changed = 'server_user_changed';
+  
+  public static server_user_items_changed = 'server_user_items_changed';
+
+
+
+
+  
+  
+  
+  
+  public static server_pay_state_change = 'server_pay_state_change'; 
+
+
+
+
+  
+  
+  
+  
+  public static server_hall_join_match = 'server_hall_join_match';
+  public static server_hall_quit_match = 'server_hall_quit_match';
+
+
+
+  
+  
+  
+  
+  public static server_room_quit = 'server_room_quit';
+
+  public static room_enter = 'room_enter';
+  public static server_room_ready = 'server_room_ready';
+
+
+  public static server_room_status_changed_before = 'server_room_status_changed_before';
+  public static server_room_status_changed = 'server_room_status_changed';
+
+  public static server_room_game_action_before = 'server_room_game_action_before';
+  public static server_room_game_action = 'server_room_game_action';
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+
+
+
+}
---
+++ scripts/libs/vo/Room_match.ts
@@ -0,0 +1,1 @@
+import{Room_seat}from './Room_seat';var _ = require('Underscore');export class Room_match extends Room_seat{public match_id}
